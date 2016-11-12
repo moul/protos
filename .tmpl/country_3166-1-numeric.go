3cbaf00ae83978d328d57e256aef02119530ba05
Manfred Touron <m@42.am>
2016-11-12 20:22:51 +0100

    Add Country codes (iso 3166-1 numeric) (fix #1)

---
+++ .tmpl/country_3166-1-numeric.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"text/template"
+
+	"github.com/moul/funcmap"
+)
+
+var tmplStr = `syntax = "proto3";
+
+/** Country codes - ISO 3166-1 numeric
+ *
+ * http://www.iso.org/iso/home/standards/country_codes.htm
+ */
+
+package country;
+
+enum CountryCode {
+  _ = 0; // zero vale
+
+{{range .}}{{if gt .ISO31661Numeric 0}}/**
+ * {{.Name}}
+ */
+  {{.ISO31661Alpha3}} = {{.ISO31661Numeric}};
+
+{{end}}{{end}}}
+`
+
+type country struct {
+	Name               string `json:"name"`
+	ISO31661Alpha3     string `json:"ISO3166-1-Alpha-3"`
+	ISO31661NumericStr string `json:"ISO3166-1-numeric"`
+	ISO31661Numeric    int
+}
+
+type countries []country
+
+func main() {
+	inputStr, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	var input countries
+	if err := json.Unmarshal(inputStr, &input); err != nil {
+		panic(err)
+	}
+
+	for idx, country := range input {
+		input[idx].ISO31661Numeric, _ = strconv.Atoi(country.ISO31661NumericStr)
+	}
+
+	tmpl, err := template.New("").Funcs(funcmap.FuncMap).Parse(tmplStr)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tmpl.Execute(os.Stdout, input); err != nil {
+		panic(err)
+	}
+}
--- Makefile
+++ Makefile
@@ -1,15 +1,26 @@
 TMP ?=	.tmp
 
+all: currency.proto country.proto
+
 currency.proto: $(TMP)/currency_4217.json
 	cat $< | go run .tmpl/currency_4217.go > $@.tmp
 	mv $@.tmp $@
 
+country.proto: $(TMP)/country-codes.json
+	cat $< | go run .tmpl/country_3166-1-numeric.go > $@.tmp
+	mv $@.tmp $@
+
+$(TMP)/country-codes.json:
+	@mkdir -p $(TMP)
+	wget -O $@ http://data.okfn.org/data/core/country-codes/r/country-codes.json
+
 $(TMP)/currency_4217.json: $(TMP)/currency_4217.xml
+	@mkdir -p $(TMP)
 	cat $< | xml2json > $@.tmp
 	mv $@.tmp $@
 
 $(TMP)/currency_4217.xml:
-	mkdir -p .tmp
+	@mkdir -p $(TMP)
 	wget http://www.currency-iso.org/dam/downloads/lists/list_one.xml -O$@
 
 
